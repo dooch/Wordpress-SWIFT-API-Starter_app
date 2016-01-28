c0a7ad1aa715113a4722ee9a35229d492ed1e369
theDooch <chrisducci@gmail.com>
2016-01-28 13:37:20 +1100

    Initial Commit

---
+++ JSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON
+//
+//  Created by CB on 28/01/2016.
+//  Copyright © 2016 streetartcritic.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
