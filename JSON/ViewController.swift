b6a3cf4547131cad2358f46089dbddb7e8c94cfd
theDooch <cb@CBs-MacBook-Pro.local>
2016-02-01 20:55:44 +1100

    Creating a Wordpress Application using SWIFT, that can call Wordpress APIs
    This is my first commit for this project, so lets see how it goes.

--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -7,13 +7,73 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var NumberofRows = 0
+    var NamesArray = [String]()
+    var AgeArray = [String]()
+    
+    //Adding in the code
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        parseJSON()
     }
+    
+    
+    func parseJSON() {
+        
+   //   let path = NSBundle.mainBundle().pathForResource("jsonFile2", ofType: "json") as String!
+        let path = "/Users/cb/Documents/dev/GitHub/JSON/JSON/jsonFile2"
+        
+        let jsonData = NSData(contentsOfFile: path) as NSData!
+        let readableJSON = JSON(data: jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        
+        var Name = readableJSON["People"]["person1"]["Age"]
+        
+        NumberofRows = readableJSON["People"].count
+        
+        for i in 1...NumberofRows {
+            var Person = "Person"
+            Person += "\(i)"
+            var Name = readableJSON["People"][Person]["Name"].string as String!
+            var Age = readableJSON["People"][Person]["Age"].string as String!
+            NamesArray.append(Name)
+            AgeArray.append(Age)
+        }
+        
+        
+        NSLog("\(Name)")
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return NumberofRows
+        
+    }
+    
+    func callWordPressAPI() {
+        
+        
+        
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        if NamesArray.count != 0 {
+            cell.textLabel?.text = NamesArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Pods/Target Support Files/Pods-JSON/Pods-JSON-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_JSONVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_JSONVersionString[];
+
